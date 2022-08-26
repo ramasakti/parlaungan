6edbcf5e3e8aacdc31b26fc69b771df73789f613
ramasakti27 <ramasakti1337@gmail.com>
2022-08-26 13:53:58 +0700

    update fitur jadwal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,67 @@
 @extends('layout.navbar')
 @extends('layout.header')
 @section('konten')
-    
+<main>
+    <div class="container py-4">
+      <header class="pb-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+          <img src="/img/smaispa.png" width="40" height="39" class="me-2" viewbox="0 0 118 94" role="img">
+          <span class="fs-4">Selamat Datang!</span>
+        </a>
+      </header>
+  
+      <div id="carouselExampleIndicators" class="carousel slide mb-4" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class=""></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/img/front/1.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="/img/front/2.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="/img/front/3.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <section class="py-4 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-6 mx-auto">
+            <h1 class="fw-light">SMA Islam Parlaungan</h1>
+            <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+          </div>
+        </div>
+      </section>
+  
+      <div class="row align-items-md-stretch">
+        <div class="col-md-6">
+          <div class="h-100 p-5 text-white bg-dark rounded-3">
+            <h2>Change the background</h2>
+            <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            <button class="btn btn-outline-light" type="button">Example button</button>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="h-100 p-5 bg-light border rounded-3">
+            <h2>Add borders</h2>
+            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
+            <button class="btn btn-outline-secondary" type="button">Example button</button>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
 @endsection
