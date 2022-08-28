310038436c972ecefc0346362e96c822b1a2436c
ramasakti27 <ramasakti1337@gmail.com>
2022-08-29 06:28:19 +0700

    public masuk ke repo

---
+++ resources/views/jadwal/hari/kelas.blade.php
@@ -0,0 +1,8 @@
+<form action="/jadwal" method="get">
+    <select class="uk-margin uk-select" name="id_kelas" id="" onchange="this.form.submit()">
+        <option value="">Pilih Kelas</option>
+        @foreach ($dataKelas as $kelas)
+            <option value="{{ $kelas->id_kelas }}">{{ $kelas->tingkat }} {{ $kelas->jurusan }}</option>
+        @endforeach
+    </select>
+</form>
