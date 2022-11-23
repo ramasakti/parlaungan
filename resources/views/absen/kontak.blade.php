0bce51cfc721d2b536c49d15f3685d8cc87fdac6
ramasakti <ramasakti1337@gmail.com>
2022-11-23 10:55:48 +0700

    FIX

---
+++ resources/views/absen/kontak.blade.php
@@ -0,0 +1,25 @@
+<div id="kontak-{{ $siswa->id_siswa }}" class="uk-flex-top" uk-modal>
+    <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+        <button class="uk-modal-close-default" type="button" uk-close></button>
+        <h5>Detail Kontak</h5>
+        <div class="uk-margin">
+            @php
+                $regex = '/[0-9]/';
+                $result = [];
+                preg_match_all($regex, $siswa->telp, $result);
+                if ($result[0][0] == '0') {
+                    unset($result[0][0]);
+                    $nope = '62'.implode('', $result[0]);
+                }else{
+                    $nope = implode('', $result[0]);
+                }
+            @endphp
+            <p>
+                <a href="https://wa.me/{{ $nope }}">{{ $siswa->nama_siswa }}</a>
+            </p>
+            <p>
+                <a href="https://wa.me/"></a>
+            </p>
+        </div>
+    </div>
+</div>
