7b4b70f63a4df080473f5b75c3292d042c9fab73
ramasakti <ramasakti1337@gmail.com>
2023-04-30 01:08:15 +0700

    Merapihkan View Blog

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -29,8 +29,8 @@
         <article>
 			<header class="uk-margin-medium-bottom">
 				<h1 class="uk-article-title" id="title">Judul Artikel</h1>
-				<p id="author">Penulis: John Doe</p>
-				<p id="uploaded">Tanggal: 1 Mei 2023</p>
+				<p id="author" class="uk-margin-remove-bottom">Penulis: John Doe</p>
+				<p id="uploaded" class="uk-margin-remove-top">Tanggal: 1 Mei 2023</p>
 			</header>
 
 			<div class="uk-margin-medium-bottom">
